58a9db50adf5676024f013947fca06fbf52d54e1
Tajbid <tajbidrayhan111@gmail.com>
2022-08-15 19:05:59 +0600

    content re-modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  _content: ["./src/**/*.{html,js}"],
+  get content() {
+    return this._content;
+  },
+  set content(value) {
+    this._content = value;
+  },
   theme: {
     extend: {},
   },
